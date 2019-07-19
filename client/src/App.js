7b9052764d760d6541275e9d8d464a1fa1ac7c51
Jason Aviles <sandman2k18@outlook.com>
2019-07-19 14:31:46 -0400

    done

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import Login from './components/Login'
 import {Link,Route} from 'react-router-dom'
 import './App.css';
@@ -6,18 +6,38 @@ import Reg from './components/Reg'
 import LoginOut from './components/LoginOut'
 import Data from './components/data/DataList'
 function App() {
+const [register,setRegister] =useState('register')
+const token = localStorage.getItem("token");
 
-  const token = localStorage.getItem("token");
+const changeReg=() =>{
+if(token) {setRegister('users')}else{setRegister('register')}
+}
+useEffect(()=>{changeReg()},[])
+
+  
   return (
     <div className="App">
 
 <header>
-{token ?<Link to='/loginOut'><Route exact path='/loginOut' component={LoginOut}/></Link>: <Link to='/login'>login</Link> }
 
-<Link to='/register'>reg</Link>
-<Link to='/user'>users</Link>
+<div className="ui menu">
+  <div className="item">
+    
+{token ?<Link  className="ui button" to='/user'>{register}</Link>: <Link className="ui primary button" to='/register'>{register}</Link> }
+  </div>
+  <div className="item">
+    {token ?<Link className="ui button" to='/loginOut'>LogOut</Link>: <Link  className="ui button" to='/login'>login</Link> }
+  </div>
+
+ 
+</div>
+
+
+
+
+
 </header>
-
+<Route exact path='/loginOut' component={LoginOut}/>
 <Route exact path='/login' component={Login}/>
 <Route exact path='/register'     component={Reg}/>
 <Route exact path='/user'     component={Data}/>
